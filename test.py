373c2d54c45cadccc67c355acaef8440c9b7ddd0
tomiharu0317 <tomiharu1017@gmail.com>
2020-05-10 16:31:53 +0900

    scraping test using Requests and Beautifulsoup4

--- test.py
+++ test.py
@@ -1,3 +1,17 @@
-def hello():
-    print("hello world")
-    print("bad line")
+import requests
+from bs4 import BeautifulSoup
+import re
+
+url = "https://news.yahoo.co.jp"
+response = requests.get(url)
+some_text = response.text[:500]
+print(some_text)
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+elems = soup.find_all(href=re.compile("news.yahoo.co.jp/pickup"))
+print(elems,'\n')
+
+for elem in elems:
+    print(elem.contents[0])
+    print(elem.attrs['href'])
